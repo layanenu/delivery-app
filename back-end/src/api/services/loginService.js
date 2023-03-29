9a6a5a9c1e37f6485ad5fb09678a57af8046796c
Aline Grance <grance.aline@gmail.com>
2023-03-29 16:23:17 -0300

    Feat: jwt config

--- back-end/src/api/services/loginService.js
+++ back-end/src/api/services/loginService.js
@@ -1,8 +1,15 @@
+const md5 = require('md5');
 const { User } = require('../../database/models');
+const { tokenGenerate } = require('../utils/JWT');
 
-const login = async (email, _password) => {
+const login = async (email, password) => {
   const user = await User.findOne({ where: { email } });
-  return user;
+  if (!user || user.password === md5(password)) {
+    return null;
+  }
+  const token = tokenGenerate(user);
+
+  return token;
 };
 
 module.exports = { login };
