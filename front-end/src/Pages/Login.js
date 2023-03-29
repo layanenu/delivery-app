4cc39f47c56064a356cbb79aa5d74877a75c8431
GabrielRodriguesMachado <gabrm123@gmail.com>
2023-03-29 16:00:51 -0300

    requisito 3 concluido

--- front-end/src/Pages/Login.js
+++ front-end/src/Pages/Login.js
@@ -1,8 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 function Login() {
   const history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const commonLogin = 'common_login__';
   const inputEmail = 'input-email';
   const inputPassword = 'input-password';
@@ -10,17 +13,36 @@ function Login() {
   const buttonRegister = 'button-register';
   const elementInvalidEmail = 'element-invalid-email';
 
+  const minPasswordLenght = 6;
+  let emailIsValid = email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+
   useEffect(() => {
     history.push('/login');
   }, []);
+
+  useEffect(() => {
+    emailIsValid = email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+  }, [email]);
   return (
     <div>
       <form>
-        <input type="text" data-testid={ `${commonLogin}${inputEmail}` } />
-        <input type="text" data-testid={ `${commonLogin}${inputPassword}` } />
+        <input
+          type="email"
+          data-testid={ `${commonLogin}${inputEmail}` }
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+        <input
+          type="password"
+          data-testid={ `${commonLogin}${inputPassword}` }
+          value={ password }
+          onChange={ (e) => setPassword(e.target.value) }
+        />
         <button
           type="button"
           data-testid={ `${commonLogin}${buttonLogin}` }
+          value={ email }
+          disabled={ !(emailIsValid && password.length >= minPasswordLenght) }
         >
           LOGIN
         </button>
