f89d90fe0d69ab4ed4faf7520f8d4ca8bcb4c362
Leonardo Kepler <leokepler@gmail.com>
2023-04-05 16:38:32 -0300

    fix: role validation

--- back-end/src/api/middlewares/roleValidation.js
+++ back-end/src/api/middlewares/roleValidation.js
@@ -1,15 +1,17 @@
 const roleValidation = async (req, res, next) => {
   const { role } = req.body;
   const validRoles = ['customer', 'seller', 'administrator'];
+
   if (!role) {
     req.body.role = 'customer';
+    return next();
   }
 
   if (validRoles.includes(role)) {
     return next();
   }
-  
-  return res.sendStatus(400);
+
+  return res.status(400).json({ message: 'Role inválida' });
 };
 
 module.exports = { roleValidation };
