cf33921898e12c218071ec2b011c055498b757cf
Aline Grance <grance.aline@gmail.com>
2023-04-05 22:00:33 -0300

    Feat req 30, iniciando

--- front-end/src/Pages/SellerOrder.js
+++ front-end/src/Pages/SellerOrder.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import Navbar from '../components/Navbar';
+import { requestData } from '../services/request';
 
-function SellerOrder() {
+function SellerOrder({ match: { params: { id } } }) {
+  const [order, setOrder] = useState([]);
+
+  useEffect(() => {
+    async function fetchOrder() {
+      const response = await requestData(`/sales/${id}`);
+      setOrder(response);
+      console.log(response);
+    }
+    fetchOrder();
+    console.log(order);
+  }, []);
+
   return (
-    <div>SellerOrder</div>
+    <div>
+      <Navbar />
+    </div>
+
   );
 }
 
 export default SellerOrder;
+
+SellerOrder.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
